8c78aa6a92434990c5463d4465bd191384e5da57
unknown <maldinuribrahim@gmail.com>
2019-05-15 02:08:02 +0700

    Register Menu

---
+++ Resources/views/admin/v_1/content/sidebar.blade.php
@@ -0,0 +1,12 @@
+<li class="m-menu__item  {{url()->full() == action('\Gdevilbat\SpardaCMS\Modules\Account\Http\Controllers\AccountController@index') ? 'm-menu__item--active' : ''}}" aria-haspopup="true"}>
+    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Account\Http\Controllers\AccountController@index')}}" class="m-menu__link ">
+        <i class="m-menu__link-icon flaticon-user-settings"></i>
+        <span class="m-menu__link-title"> 
+            <span class="m-menu__link-wrap"> 
+                <span class="m-menu__link-text">
+                    Account
+                </span>
+             </span>
+         </span>
+     </a>
+</li>
